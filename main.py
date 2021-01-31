a303b8d6332a30e5d9d9fc49e38e112713ddbb98
Gentris <lecigentris@gmail.com>
2021-01-31 16:47:32 +0100

    Add optimal solution

--- main.py
+++ main.py
@@ -3,8 +3,30 @@ def get_max_subsequence(array: list, size: int) -> tuple:
 	end: int = 0
 	max: int = array[0]
 
-	sum = array[0]
-	sum_start_index = 0;
+	sum: list = [array[0]]
+	sum_start_index: int = 0;
+
+	for i in range(1, size):
+		if (sum[i - 1] > 0):
+			sum.insert(i, sum[i - 1] + array[i])
+		else:
+			sum.insert(i, array[i])
+			sum_start_index = i
+
+		if (sum[i] > max):
+			start = sum_start_index
+			end = i
+			max = sum[i]
+	
+	return (max, start, end)
+
+def get_max_subsequence_by_optimal_solution(array: list, size: int) -> tuple:
+	start: int = 0
+	end: int = 0
+	max: int = array[0]
+
+	sum: int = array[0]
+	sum_start_index: int = 0;
 
 	for i in range(1, size):
 		if (sum > 0):
